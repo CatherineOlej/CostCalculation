30c43236a2357dd6bb76b5e1768a9d27b1936a8b
CatherineOlej <37804018+CatherineOlej@users.noreply.github.com>
2020-10-09 00:37:41 -0400

    finalized code with comments

--- CostCalculation/Program.cs
+++ CostCalculation/Program.cs
@@ -1,40 +1,26 @@
 ﻿using System;
-//this is a practice project to do an order calculation for a delivery order online
+/*CostCalculation for delivery orders
+ *This console application is a practice project that acts as a order calculator for a food delivery service
+ *User will be asked to provide their personal information including their name and address before completing the order
+ * Author: Catherine Olejarczyk
+ * Created October 5th 2020 - Revised: October 8th 2020
+*/
 namespace CostCalculation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //strings
-            string fullName; //first and last
+
+            string fullName;
             string address;
             string areaLocation; //city, province, postal code
             string instructions;
+            double phoneNum;
+
+            decimal[] reciept = {0,0,0,0,0 };
             
-            //string numOfOrders; //item amount
-            
-
-
-            //double
-            double phoneNum = 0;
-            
-            //double studentDiscount = 10; 
-            //decimal orderTotalS = 0;
-            decimal Total = 0;
-
-
-            decimal savings = 0;
-            decimal subTotal = 0;
-            decimal taxAmount = 0;
-            //int
-            int itemAmount = 0;
-            //decimal
-            
-
-            //string isStudent;
-            //string dinner;
-
+            Console.ForegroundColor = ConsoleColor.Magenta;//changes color of text
             //intro to project
             Console.WriteLine("Welcome to Arnold's Amazing Eats. This program is designed to take customers food orders and process them. " +
                 "After the users personal information is taken along with there order, the recipt will display back" +
@@ -52,94 +38,37 @@ namespace CostCalculation
             Console.WriteLine("Please provide special instructions for deliveries from this address");
             instructions = Console.ReadLine();
 
-
+            //now ordering begins
             Console.WriteLine("Great! Now that we have your delivery information lets get into ordering some food");
             double choice;
             do
             {
                 Console.WriteLine("Select option 1 or 2");
                 Console.WriteLine("1) Burger combo | 15.99 (Burger with a side of fries and choice of drink)");
-                Console.WriteLine("2) Steak combo | 25.45 (Steak how you like it with a side of veggies and baked potato with choice of drink)");
+                Console.WriteLine("2) Steak combo | 25.50 (Steak how you like it with a side of veggies and baked potato with choice of drink)");
                 Console.WriteLine("");
 
                 choice = int.Parse(Console.ReadLine());
 
-                if (choice == 1)
+                if (choice == 1) //if user selects option 1 then execute method
                 {
-                    BurgerCombo();
+                    reciept = BurgerCombo();
+                    break;
                 }
-                else if (choice == 2)
+                else if (choice == 2) //if user selects option 2 then execute method
                 {
-                    SteakCombo();
+                    reciept = SteakCombo();
+                    break;
                 }
-                else
+                else //try again
                 {
                     Console.WriteLine("Invalid choice");
                     Console.WriteLine("Please specify from the menu");
                 }
 
 
-            } while (choice !=3);
+            } while (choice == 1 || choice == 2);
 
-        
-            //Console.WriteLine("");
-            //Console.WriteLine("Is this correct? (Y/N): " + itemAmount); //confirm amount
-            //optionOne = Console.ReadLine();
-            //optionTwo = Console.ReadLine();
-            
-        static void BurgerCombo()
-        {
-                decimal HST = 1.13M;
-                string Student;
-                
-                decimal Total = 0;
-                decimal savings = 0;
-                decimal subTotal = 0;
-                decimal taxAmount = 0;
-                //int
-                int itemAmount = 0;
-                //decimal
-                decimal priceComboOne = 0;
-
-                //int itemAmount = 0;
-                Console.WriteLine("How many of those meals are being ordered today?"); //ask how many they want
-                string numOfOrders = Console.ReadLine(); //converting string to int
-                itemAmount = int.Parse(numOfOrders);
-                Console.WriteLine("Is this correct? (Y/N): " + itemAmount); //confirm amount
-                string answer = Console.ReadLine();
-                
-                if (answer == "y" || answer == "Y")
-                {
-                    Console.WriteLine("Are you a student? (Y/N)");
-                    Student = Console.ReadLine();
-
-                    if (Student == "y" || Student == "Y")
-                    {
-                        decimal d = 15.99M;
-                        priceComboOne = d * itemAmount; //price of option 1
-                        //int discount = 10; //dicount amount
-                        savings = priceComboOne * 0.1M; //calculate discount savings
-                        Math.Round(savings, 2); //round 2 decimal places
-                        subTotal = priceComboOne - savings; //sub total is a result from the price of the item minus the discount
-                        taxAmount = (subTotal * HST) - subTotal; //calculate the exact tax amount in decimal format and round
-                        Math.Round(taxAmount, 2);
-                        Total = subTotal + taxAmount;
-
-                    }
-                    else if (Student == "n" || Student == "N")
-                    {
-                        subTotal = priceComboOne * itemAmount; //sub total is the price of the item 
-                        taxAmount = (subTotal * HST) - subTotal; //calculate the exact tax amount in decimal format and round
-                        Math.Round(taxAmount, 2);
-                        Total = priceComboOne + subTotal + taxAmount;
-
-                    }
-                }
-                else
-                {
-
-                }
-            }
             Console.Clear();
             //Data recipt of the order
             Console.WriteLine("");
@@ -155,123 +84,124 @@ namespace CostCalculation
             Console.WriteLine("");
             Console.WriteLine("------------------");
             Console.WriteLine("");
-            Console.WriteLine("Item Amt: " + itemAmount);
+            Console.WriteLine("Item Amt: " + reciept[3]);
             Console.WriteLine("Name of the dinner: " + choice);
             Console.WriteLine("");
-            Console.WriteLine("10% student savings: " + savings);
-            Console.WriteLine("Sub Total: " + subTotal);
-            Console.WriteLine("Tax (13%): " + taxAmount);
+            Console.WriteLine("10% student savings: " + reciept[1]);
+            Console.WriteLine("Sub Total: " + reciept[4]);
+            Console.WriteLine("Tax (13%): " + reciept[2]);
             Console.WriteLine("");
-            Console.WriteLine("TOTAL: " + Total);
-
+            Console.WriteLine("TOTAL: " + reciept[0]);
             Console.ReadKey(true);
-
         }
+    
 
-
-
-            static void SteakCombo()
+        public static decimal[] BurgerCombo()
         {
-
-        }
-
-        //if (optionOne == "1") //if the first option is selected
-        //{
-        //    //Console.WriteLine("How many of those meals are being ordered today?"); //ask how many they want
-        //    numOfOrders = Console.ReadLine(); //converting string to int
-        //    itemAmount = int.Parse(numOfOrders);
-
-        //    if (numOfOrders == "y" || numOfOrders == "Y")
-        //    {
-        //        Console.WriteLine("Are you a student? (Y/N)");
-        //        Student = Console.ReadLine();
-
-        //        if (Student == "y" || Student == "Y")
-        //        {
-        //            priceComboOne = 15.99M + itemAmount; //price of option 1
-        //            int discount = 10; //dicount amount
-        //            savings = priceComboOne * discount / 100; //calculate discount savings
-        //            Math.Round(savings, 2); //round 2 decimal places
-        //            subTotal = priceComboOne - savings; //sub total is a result from the price of the item minus the discount
-        //            taxAmount = (subTotal * HST) - subTotal; //calculate the exact tax amount in decimal format and round
-        //            Math.Round(taxAmount, 2);
-        //            orderTotalS = priceComboOne + savings + subTotal + taxAmount;
-
-        //            Console.WriteLine("10% student saving" + savings);
-        //        }
-        //        else if (Student == "n" || Student == "N")
-        //        {
-        //            subTotal = priceComboOne + itemAmount; //sub total is the price of the item 
-        //            taxAmount = (subTotal * HST) - subTotal; //calculate the exact tax amount in decimal format and round
-        //            Math.Round(taxAmount, 2);
-        //            orderTotal = priceComboOne + subTotal + taxAmount;
-
-        //        }
-        //    }
-
-        //}
-
-        //else if (optionTwo == "2")
-        //{
-        //    Console.WriteLine("How many of those meals are being ordered today?");
-        //    numOfOrders = Console.ReadLine();
-        //    itemAmount = int.Parse(numOfOrders);
-
-        //    Console.WriteLine("");
-        //    Console.WriteLine("Is this correct? (Y/N): " + itemAmount); //confirm amount
-
-        //    if (numOfOrders == "y" || numOfOrders == "Y")
-        //    {
-        //        Console.WriteLine("Are you a student? (Y/N)");
-        //        Student = Console.ReadLine();
-
-        //        if (Student == "y" || Student == "Y")
-        //        {
-        //            priceComboTwo = 25.45M; //price of option 2
-        //            int discount = 10; //dicount amount
-        //            savings = priceComboTwo * discount / 100; //calculate discount savings
-        //            Math.Round(savings, 2); //round 2 decimal places
-        //            subTotal = priceComboTwo - savings; //sub total is a result from the price of the item minus the discount
-        //            taxAmount = (subTotal * HST) - subTotal; //calculate the exact tax amount in decimal format and round
-        //            Math.Round(taxAmount, 2);
-        //            orderTotal = priceComboTwo + savings + subTotal + taxAmount;
-        //        }
-        //        else if (Student == "n" || Student == "N")
-        //        {
-        //            subTotal = priceComboTwo; //sub total is the price of the item 
-        //            taxAmount = (subTotal * HST) - subTotal; //calculate the exact tax amount in decimal format and round
-        //            Math.Round(taxAmount, 2);
-        //            orderTotal = priceComboTwo + subTotal + taxAmount;
-
-        //        }
-        //    }
-        //}
-        //Console.Clear();
-        //    //Data recipt of the order
-        //    Console.WriteLine("");
-        //    Console.WriteLine(fullName);
-        //    Console.WriteLine("");
-        //    Console.WriteLine(address);
-        //    Console.WriteLine("");
-        //    Console.WriteLine(areaLocation);
-        //    Console.WriteLine("");
-        //    Console.WriteLine(phoneNum);
-        //    Console.WriteLine("");
-        //    Console.WriteLine(instructions);
-        //    Console.WriteLine("");
-        //    Console.WriteLine("------------------");
-        //    Console.WriteLine("");
-        //    Console.WriteLine("Item Amt: " + itemAmount);
-        //    Console.WriteLine("Name of the dinner: " + choice);
-        //    Console.WriteLine("");
-        //    Console.WriteLine("10% student savings: " + savings);
-        //    Console.WriteLine("Sub Total: " + subTotal);
-        //    Console.WriteLine("Tax (13%): " + taxAmount);
-        //    Console.WriteLine("");
-        //    Console.WriteLine("TOTAL: " + Total);
+            int itemAmount = 0;
+            string Student; 
             
+            decimal HST = 1.13M;
+            decimal Total = 0;
+            decimal savings = 0;
+            decimal subTotal = 0;
+            decimal taxAmount = 0;
+            decimal priceComboOne = 0;
 
-        //    Console.ReadKey(true);
-        }
+            Console.WriteLine("How many of those meals are being ordered today?"); //ask how many they want
+            string numOfOrders = Console.ReadLine(); //converting string to int
+            itemAmount = int.Parse(numOfOrders);
+            Console.WriteLine("Is this correct? (Y/N): " + itemAmount); //confirm amount
+            string answer = Console.ReadLine();
+
+            while (answer == "y" || answer == "Y") //calculations will take place once order is confirmed
+            {
+                Console.WriteLine("Are you a student? (Y/N)"); //if the customer is a student then a discount will apply
+                Student = Console.ReadLine();
+
+                if (Student == "y" || Student == "Y") //if student do the following
+                {
+                    decimal d = 15.99M; //price of option 1
+                    priceComboOne = d * itemAmount; 
+                    savings = priceComboOne * 0.1M; //calculate discount savings
+                    savings = Math.Round(savings, 2); //round 2 decimal places
+                    subTotal = priceComboOne - savings; //sub total is a result from the price of the item minus the discount
+                    taxAmount = (subTotal * HST) - subTotal; //calculate the exact tax amount in decimal format and round
+                    taxAmount = Math.Round(taxAmount, 2);
+                    Total = subTotal + taxAmount;
+                    break;
+
+                }
+                else if (Student == "n" || Student == "N") //if not a student do the following
+                {
+                    decimal d = 15.99M; //price of option 1
+                    subTotal = d * itemAmount; //sub total is the price of the item 
+                    taxAmount = (subTotal * HST) - subTotal; //calculate the exact tax amount in decimal format and round
+                    taxAmount = Math.Round(taxAmount, 2);
+                    Total = subTotal + taxAmount;
+                    break;
+
+                }
+            }
+            decimal[] arr2 = {Total, savings, taxAmount, itemAmount, subTotal };
+            return arr2;
+        } 
+
+            public static decimal[] SteakCombo()
+            {
+                int itemAmount = 0;
+                string Student;
+
+                decimal HST = 1.13M;
+                decimal Total = 0;
+                decimal savings = 0;
+                decimal subTotal = 0;
+                decimal taxAmount = 0;
+                decimal priceComboTwo = 0;
+
+                Console.WriteLine("How many of those meals are being ordered today?"); //ask how many they want
+                string numOfOrders = Console.ReadLine(); //converting string to int
+                itemAmount = int.Parse(numOfOrders);
+                Console.WriteLine("Is this correct? (Y/N): " + itemAmount); //confirm amount
+                string answer = Console.ReadLine();
+
+                while (answer == "y" || answer == "Y") //calculations will take place once order is confirmed
+            {
+                    Console.WriteLine("Are you a student? (Y/N)");
+                    Student = Console.ReadLine();
+
+                    if (Student == "y" || Student == "Y") //if student do the following
+                    {
+                        decimal d = 25.50M; //price of option 2
+                        priceComboTwo = d * itemAmount; 
+                        savings = priceComboTwo * 0.1M; //calculate discount savings
+                        savings = Math.Round(savings, 2); //round 2 decimal places
+                        subTotal = priceComboTwo - savings; //sub total is a result from the price of the item minus the discount
+                        taxAmount = (subTotal * HST) - subTotal; //calculate the exact tax amount in decimal format and round
+                        taxAmount = Math.Round(taxAmount, 2);
+                        Total = subTotal + taxAmount;
+                        break;
+
+                    }
+                    else if (Student == "n" || Student == "N") //if not a student do the following
+                    {
+                        decimal d = 25.50M; //price of option 2
+                        subTotal = d * itemAmount; //sub total is the price of the item 
+                        taxAmount = (subTotal * HST) - subTotal; //calculate the exact tax amount in decimal format and round
+                        taxAmount = Math.Round(taxAmount, 2);
+                        Total = subTotal + taxAmount;
+                        break;
+
+                    }
+                }
+            decimal[] arr2 = { Total, savings, taxAmount, itemAmount, subTotal };
+            return arr2;
+            }
     }
+}
+
+
+        
+    
+    
 
